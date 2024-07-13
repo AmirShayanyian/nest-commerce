f2bbaa760ca940c83ccc65e767ea7429942efec0
amirshayanyian <wahtamir12@gmail.com>
2024-07-13 10:43:50 +0330

    entity and controller name enum made

---
+++ src/common/enums/controller.enum.ts
@@ -0,0 +1,3 @@
+export enum ControllerName {
+  Auth = 'auth',
+}
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ControllerName } from 'src/common/enums/controller.enum';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('auth')
+@ApiTags('Auth')
+@Controller(ControllerName.Auth)
 export class AuthController {
   constructor(private authService: AuthService) {}
 }
