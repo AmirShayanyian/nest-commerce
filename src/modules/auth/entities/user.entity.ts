05dd12639e1331fee972e9cbc3b29052ee1d990e
amirshayanyian <wahtamir12@gmail.com>
2024-07-13 10:47:07 +0330

    filled user entity and injected in the module

--- src/modules/auth/entities/user.entity.ts
+++ src/modules/auth/entities/user.entity.ts
@@ -1,5 +1,21 @@
+import { BaseEntity } from 'src/common/abstracts/base.entity';
 import { EntityName } from 'src/common/enums/entity.enum';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity(EntityName.User)
-export class UserEntity {}
+export class UserEntity extends BaseEntity {
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+}
