84db09475945c2a6595848e656d591896b613e95
amirshayanyian <wahtamir12@gmail.com>
2024-07-13 11:38:49 +0330

    made custom validation messages

---
+++ src/common/enums/messages.enum.ts
@@ -0,0 +1,6 @@
+export enum SignInValidation {
+  PasswordLength = 'Password must be between 8 - 32',
+  PasswordNotEmpty = 'Password field cannot be empty',
+  UsernameLength = 'Username must be between 5 - 25',
+  UsernameNotEmpty = 'Username field cannot be empty',
+}
--- src/modules/auth/dtos/sign-up.dto.ts
+++ src/modules/auth/dtos/sign-up.dto.ts
@@ -1,14 +1,15 @@
 import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import { SignInValidation } from 'src/common/enums/messages.enum';
 
 export class SignInDto {
-  @IsNotEmpty()
+  @IsNotEmpty({ message: SignInValidation.UsernameNotEmpty })
   @IsString()
-  @Length(3, 25)
+  @Length(3, 25, { message: SignInValidation.UsernameLength })
   username: string;
 
+  @IsNotEmpty({ message: SignInValidation.PasswordNotEmpty })
   @IsString()
-  @IsNotEmpty()
-  @Length(8, 32)
+  @Length(8, 32, { message: SignInValidation.PasswordLength })
   password: string;
 
   @IsString()
