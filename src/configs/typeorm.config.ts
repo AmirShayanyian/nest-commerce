5b45529c6343a8e8e460953bc066ae9d173faa9a
amirshayanyian <wahtamir12@gmail.com>
2024-07-21 15:53:30 +0330

    added migration setup to project

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,27 +1,17 @@
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
 export class TypeOrmConfigs implements TypeOrmOptionsFactory {
-  createTypeOrmOptions(
-    connectionName?: string
-  ): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
-    const {
-      DB_AUTO_LOAD,
-      DB_SYNC,
-      DB_HOST,
-      DB_PORT,
-      DB_USERNAME,
-      DB_PASSWORD,
-      DB_DATABASE,
-    } = process.env;
+  createTypeOrmOptions(connectionName?: string): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
     return {
       type: 'mysql',
       host: DB_HOST,
-      port: 4942,
+      port: +DB_PORT,
       username: DB_USERNAME,
       database: DB_DATABASE,
       password: DB_PASSWORD,
-      synchronize: true,
-      autoLoadEntities: true,
+      synchronize: false,
+      autoLoadEntities: false,
     };
   }
 }
